383b625d3304b39ed078dddfccc63bb7bacbead1
Marco Forte <fortemarco.irl@gmail.com>
2014-08-08 15:02:12 +0100

    Update and rename testAtoD_ReadVoltage.py to exampleAtoD_ReadVoltage.py

--- exampleAtoD_ReadVoltage.py
+++ exampleAtoD_ReadVoltage.py
@@ -1,6 +1,7 @@
-﻿from __future__ import print_function
+from __future__ import print_function
 
 # Example program to show how to read from a single AtoD channel with default settings
+# Should print out the correct voltage value for channel 0
 # Marco Forte, 18/06/2014
 
 from atod import AtoD
