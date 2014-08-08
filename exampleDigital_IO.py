a5450da9b3e9aa30e9244f40e048d56c53e8e293
Marco Forte <fortemarco.irl@gmail.com>
2014-08-08 15:16:38 +0100

    Update and rename testDigital_IO.py to exampleDigital_IO.py

--- exampleDigital_IO.py
+++ exampleDigital_IO.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+# Block below for Python 2&3 support
 try:
     input = raw_input
 except NameError:
     pass
+
 # Marco Forte, 18/06/2014
-# Test Digital IO by writing across both ports 0 and 1 of the Usb6008
+# Example of using Digital IO to  write across both ports 0 and 1 of the Usb6008
 
 from digital_io import Digital_IO
-
-# Default options are output and
 
 myDigital_IO = Digital_IO()
 while True:
