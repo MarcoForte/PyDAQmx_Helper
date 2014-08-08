8594ce14da080d667e844965598e6e0ff4a91511
Marco Forte <fortemarco.irl@gmail.com>
2014-08-08 15:00:17 +0100

    Update and rename testAtoD_PlotSample.py to exampleAtoD_PlotSample.py

--- exampleAtoD_PlotSample.py
+++ exampleAtoD_PlotSample.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 # Marco Forte, 19/06/2014
 # Read in analog signal from 1 channel and plot it
 # using oscilloscope, ramp or sine 2.2Hz, 1.41Vpp get good picture
+# Should display a nice looking sine wave plot 
 
 import numpy
 import matplotlib.pyplot as plt
