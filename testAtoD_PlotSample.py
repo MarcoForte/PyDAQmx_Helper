5986f9430d5865332cb5fc01d03b2a94d193b563
Marco Forte <fortemarco.irl@gmail.com>
2014-08-08 14:41:48 +0100

    Update and rename testAtoD4.py to testAtoD_PlotSample.py

--- testAtoD_PlotSample.py
+++ testAtoD_PlotSample.py
@@ -5,7 +5,6 @@ from __future__ import print_function
 # Marco Forte, 19/06/2014
 # Read in analog signal from 1 channel and plot it
 # using oscilloscope, ramp or sine 2.2Hz, 1.41Vpp get good picture
-#
 
 import numpy
 import matplotlib.pyplot as plt
