93fc053d9acb00ceb9bca639287ee6960b9c8c5f
Marco Forte <fortemarco.irl@gmail.com>
2014-08-11 13:55:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(
-    name='PyDAQmx-Helper',
+    name='PyDAQmx_Helper',
     version='0.1.0',
     author='Marco Forte',
     author_email='fortemarco.irl@gmail.com',
-    packages=['pydaqmx-helper', 'pydaqmx-helper.test'],
+    packages=['pydaqmx_helper', 'pydaqmx_helper.test'],
     license='LICENSE.txt',
     description='Python classes to help with everyday PyDAQmx tasks',
     long_description=open('README.txt').read(),
