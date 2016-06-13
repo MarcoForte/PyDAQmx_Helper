8a1718e65dac859e31e434e4eb9cb594f5512bdc
DiarmuiF <dirmuid.f@gmail.com>
2016-06-13 16:42:24 +0100

    getDeviceName fnc
    Added getDeviceName function to folder to allow other files to call it

---
+++ pydaqmx_helper/getDeviceName.py
@@ -0,0 +1,6 @@
+def getDeviceName(self):
+	a = c_char_p(b' ')
+	# Buffer will break if too many devices, like 10 or 15, make number 100 bigger if you have to
+	b = c_ulong(100)
+	DAQmxGetSysDevNames(a, b)
+	return a.value.decode('utf-8').split(',')[-1].strip()
