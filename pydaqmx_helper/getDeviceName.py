f8f67d3dc7aad9aeb5088a7509eeccb90e4fc585
DiarmuiF <dirmuid.f@gmail.com>
2016-06-14 11:56:11 +0100

    Self

--- pydaqmx_helper/getDeviceName.py
+++ pydaqmx_helper/getDeviceName.py
@@ -1,6 +1,6 @@
 from PyDAQmx.DAQmxFunctions import *
 
-def getDeviceName(self):
+def getDeviceName():
 	a = c_char_p(b' ')
 	# Buffer will break if too many devices, like 10 or 15, make number 100 bigger if you have to
 	b = c_ulong(100)
