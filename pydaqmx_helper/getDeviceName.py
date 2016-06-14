5afbb826291235fa6487dc2b775100d16008c885
DiarmuiF <dirmuid.f@gmail.com>
2016-06-14 16:27:21 +0100

    Renamed change "buffer" to "a"

--- pydaqmx_helper/getDeviceName.py
+++ pydaqmx_helper/getDeviceName.py
@@ -1,7 +1,7 @@
 from PyDAQmx.DAQmxFunctions import *
 
 def getDeviceName():
-	buffer = create_string_buffer(1024)
+	a = create_string_buffer(1024)
 	# Buffer will break if too many devices, like 10 or 15, make number 100 bigger if you have to
 	b = c_ulong(1024)
 	DAQmxGetSysDevNames(a, b)
