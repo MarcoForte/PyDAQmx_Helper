5c963691d3af4cddb0e3090a709110573ac97f6a
DiarmuiF <dirmuid.f@gmail.com>
2016-06-14 16:17:11 +0100

    Reassign a variable following previous commit

--- pydaqmx_helper/getDeviceName.py
+++ pydaqmx_helper/getDeviceName.py
@@ -4,5 +4,5 @@ def getDeviceName():
 	buffer = create_string_buffer(1024)
 	# Buffer will break if too many devices, like 10 or 15, make number 100 bigger if you have to
 	b = c_ulong(1024)
-	DAQmxGetSysDevNames(a, b)
+	DAQmxGetSysDevNames(buffer, b)
 	return a.value.decode('utf-8').split(',')[-1].strip()
